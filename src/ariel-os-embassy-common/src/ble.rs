f686e7b4a173e59726106f5980f5fefe161bea7d
jkr-inria <210673275+jkr-ariel@users.noreply.github.com>
2025-06-13 11:40:45 +0200

    fixup! feat(rp): expose trouBLE api to ariel-os-rp
    Signed-off-by: jkr-inria <210673275+jkr-ariel@users.noreply.github.com>

--- src/ariel-os-embassy-common/src/ble.rs
+++ src/ariel-os-embassy-common/src/ble.rs
@@ -6,6 +6,8 @@
 pub struct Config {
     /// The address of the BLE device.
     pub address: trouble_host::Address,
+    /// The Maximum Transmission Unit for the L2CAP channels
+    pub mtu: usize,
 }
 
 /// Default address, this one is used in the trouBLE examples, needs to be changed/randomized in production.
@@ -13,6 +15,7 @@ impl Default for Config {
     fn default() -> Self {
         Self {
             address: trouble_host::Address::random([0xff, 0x8f, 0x1a, 0x05, 0xe4, 0xff]),
+            mtu: 27,
         }
     }
 }
